9299fce26f575e2ad8291e63b0715e5d1c122fea
rsyeh <ryehyeh@gmail.com>
2021-06-29 15:17:12 -0400

    Added basic navigation to application, added form and landing containers

---
+++ src/containers/Landing.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Landing() {
+
+  return (
+    <div className="appForm">
+      <header className="App-header">
+      <a className="app-form-link" href="/form">
+        <button>
+          Go to form
+        </button>
+      </a>
+      </header>
+    </div>
+  );
+}
+
+export default Landing;
